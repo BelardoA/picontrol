b68920c8a34f761a9bf110224f7da880359bc55e
BelardoA <abelardo@regscale.com>
2024-01-08 20:36:15 -0500

    refactored settings to be more pythonic as well as functionality enhancements

--- picontrol/webserver/settings.py
+++ picontrol/webserver/settings.py
@@ -1,130 +1,179 @@
 #!/usr/bin/python 
-#settings.py
+# settings.py
 
-import sys, os
+import os
 from config import Config
 
-updateDir = '/home/pi/scripts/picontrol_update'
-baseDir = '/home/pi/scripts/picontrol'
 
-class Settings():
-    @staticmethod
-    def setFanSettings(fanSettings):
+class Settings:
+    def __init__(self):
+        self.update_dir = '/home/pi/scripts/picontrol_update'
+        self.base_dir = '/home/pi/scripts/picontrol'
+        self.config = Config()
+
+    def set_fan_settings(self, fan_settings: dict) -> bool:
+        """
+        Set the fan settings in the config file
+
+        :param dict fan_settings: The fan settings to save
+        :return: True if the settings were saved, False otherwise
+        :rtype: bool
+        """
         try:
-            config = Config.loadConfig()
-            config.set("fan", "thresholdOn", fanSettings['thresholdOn'])   
-            config.set("fan", "thresholdOff", fanSettings['thresholdOff'])
-            config.set("fan", "interval", fanSettings['interval'])
-
-            Config.saveConfig(config)
+            self.config.fan_settings = {
+                "thresholdOn": fan_settings['thresholdOn'],
+                "thresholdOff": fan_settings['thresholdOff'],
+                "interval": fan_settings['interval'],
+            }
+            self.config.save_config(self.config.config)
             return True
-        except:
+        except Exception as ex:
+            print('Error saving fan settings: ' + str(ex))
             return False
 
-    @staticmethod
-    def getFanSettings():
-        try:
-            config = Config.loadConfig()
-            thresholdOn = int(config.get("fan", "thresholdOn"))  
-            thresholdOff = int(config.get("fan", "thresholdOff"))  
-            interval = int(config.get("fan", "interval"))             
+    def get_fan_settings(self) -> dict:
+        """
+        Function to get fan settings from config file
 
-            return {"thresholdOn":thresholdOn, "thresholdOff":thresholdOff, "interval":interval}
-        except:
-            return {"thresholdOn":0, "thresholdOff":0, "interval":0}
-
-    @staticmethod
-    def setButtonSettings(option):
+        :returns: Dictionary containing fan settings
+        :rtype: dict
+        """
         try:
-            config = Config.loadConfig()
-            config.set("button", "option", option["option"])
-            
-            Config.saveConfig(config)
+            return self.config.fan_settings
+        except Exception as ex:
+            print("Error getting fanSettings from config: " + str(ex))
+            return {
+                "thresholdOn": 0,
+                "thresholdOff": 0,
+                "interval": 0,
+            }
+
+    def set_button_settings(self, option: dict) -> bool:
+        """
+        Function to update and save button settings in config file
+
+        :param dict option: The button option to save
+        :return: True if the settings were saved, False otherwise
+        :rtype: bool
+        """
+        try:
+            self.config.button_settings = option["option"]
+            self.config.save_config(self.config.config)
             return True
-        except:
+        except Exception as ex:
+            print('Error saving button settings: ' + str(ex))
             return False
 
-    @staticmethod
-    def getButtonSettings():
+    def get_button_settings(self) -> dict:
+        """
+        Function to get button settings from config file
+
+        :returns: Dictionary containing button settings
+        :rtype: dict
+        """
         try:
-            config = Config.loadConfig()
-            option = int(config.get("button", "option"))
+            return {"option": self.config.button_settings}
+        except Exception as ex:
+            print("Error getting button settings from config: " + str(ex))
+            return {"option": 0}
 
-            return {"option":option}
-        except:
-            return {"option":0}
+    def get_version(self) -> dict:
+        """
+        Function to get version number and date from config file
 
-    @staticmethod
-    def getVersion():
+        :returns: Dictionary containing version number and date
+        :rtype: dict
+        """
         try:
-            config = Config.loadVersion()
-           
-            number = config.get("version", "number")
-            date = config.get("version", "date")
+            return {
+                'number': self.config.version.get("version", "number"),
+                'date': self.config.version.get("version", "date"),
+            }
+        except Exception as ex:
+            print("Error getting version from config: " + str(ex))
+            return {
+                'number': '1.0',
+                'date': ''
+            }
 
-            return {'number':number, 'date':date}
-        except:
-            return {'number':'1.0', 'date':''}
+    def get_update_version(self) -> dict:
+        """
+        Function to get update version number and date from update path
 
-    @staticmethod
-    def getUpdateVersion():
+        :returns: Dictionary containing version number and date
+        :rtype: dict
+        """
         try:
-            config = Config.loadUpdateVersion()
-           
-            number = config.get("version", "number")
-            date = config.get("version", "date")
+            config = self.config.update_version
+            return {
+                'number': config.get("version", "number"),
+                'date': config.get("version", "date")
+            }
+        except Exception as ex:
+            print("Error getting update version from config: " + str(ex))
+            return {
+                'number': '1.0',
+                'date': ''
+            }
 
-            return {'number':number, 'date':date}
-        except:
-            return {'number':'1.0', 'date':''}
+    def check_updates(self) -> dict:
+        """
+        Function to check for updates
 
-    @staticmethod
-    def checkUpdates():
-        response = {"update":False}
-        try:            
-            currentVersion = Settings.getVersion()
-            
-            os.system('mkdir ' + updateDir)
+        :returns: Dictionary containing update status
+        :rtype: dict
+        """
+
+        response = {"update": False}
+        try:
+            current_version = Settings.get_version()
+
+            os.system('mkdir ' + self.update_dir)
             os.system('wget --no-check-certificate --content-disposition https://github.com/BelardoA/picontrol/raw/master/picontrol.tgz')
             os.system('tar -xzf picontrol.tgz picontrol')
-            os.system('mv ./picontrol ' + updateDir + '/picontrol')
+            os.system('mv ./picontrol ' + self.update_dir + '/picontrol')
 
-            updateVersion = Settings.getUpdateVersion()
+            update_version = Settings.get_update_version()
 
-            if currentVersion['number'] != updateVersion['number']:
-                response =  {"update":True}
+            response = {"update": current_version['number'] != update_version['number']}
 
-            os.system("sudo rm -R " + updateDir)            
-            os.system("sudo rm -R picontrol picontrol.tgz") 
-        except:
-            os.system("sudo rm -R " + updateDir) 
-            os.system("sudo rm -R picontrol picontrol.tgz") 
-            response = {"update":False}
-        
+            os.system("sudo rm -R " + self.update_dir)
+            os.system("sudo rm -R picontrol picontrol.tgz")
+        except Exception as ex:
+            print('Error when checking for updates: ' + str(ex))
+            os.system("sudo rm -R " + self.update_dir)
+            os.system("sudo rm -R picontrol picontrol.tgz")
+            response = {"update": False}
+
         return response
 
-    @staticmethod
-    def updateVersion():
+    def update_version(self) -> dict:
+        """
+        Function to check the version of the update and update the system
+
+        :returns: Dictionary containing update status
+        :rtype: dict
+        """
         response = {"update":False}
-        try:                        
-            os.system('mkdir ' + updateDir)
+        try:
+            os.system('mkdir ' + self.update_dir)
             os.system('wget --no-check-certificate --content-disposition https://github.com/BelardoA/picontrol/raw/master/picontrol.tgz')
             os.system('tar -xzf picontrol.tgz picontrol')
-            os.system('mv ./picontrol ' + updateDir + '/picontrol')
-            os.system('cp ' + baseDir + '/configs/config.conf ' + updateDir + '/picontrol/configs/config.conf')
+            os.system('mv ./picontrol ' + self.update_dir + '/picontrol')
+            os.system('cp ' + self.base_dir + '/configs/config.conf ' + self.update_dir + '/picontrol/configs/config.conf')
             print('copied config')
-            os.system('sudo rm -R ' + baseDir)
+            os.system('sudo rm -R ' + self.base_dir)
             print('deleted base')
-            os.system('cp -R ' + updateDir + '/picontrol ' + baseDir)
+            os.system('cp -R ' + self.base_dir + '/picontrol ' + self.base_dir)
             print('copied update')
 
-            os.system("sudo rm -R " + updateDir)            
+            os.system("sudo rm -R " + self.update_dir)
             os.system("sudo rm -R picontrol picontrol.tgz")
-            response = {"update":Settings.getVersion()["number"]}
-        except:
-            print('error')
-            os.system("sudo rm -R " + updateDir) 
-            os.system("sudo rm -R picontrol picontrol.tgz") 
-            response = {"update":False}
-        
+            response = {"update": Settings.get_version()["number"]}
+        except Exception as ex:
+            print('Error updating version: ' + str(ex))
+            os.system("sudo rm -R " + self.update_dir)
+            os.system("sudo rm -R picontrol picontrol.tgz")
+            response = {"update": False}
+
         return response
