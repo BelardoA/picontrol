a17d4c4f94dc0ffce51ce39adfdbf300b7c5c9af
BelardoA <livintodie4e@gmail.com>
2024-01-14 17:23:27 -0500

    added utils directory and dynamic_objects.py

---
+++ picontrol/picontrol_web_app/utils/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Utilities for PiControl web app."""
+
+from .dynamic_objects import HeaderLabel, SliderValue
---
+++ picontrol/picontrol_web_app/utils/dynamic_objects.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Custom class to allow for dynamic header labels in PiControl web app."""
+from nicegui import ui
+
+
+class HeaderLabel(ui.label):
+    """
+    This class is a subclass of NiceGUI's label class. It is used to display the header of the page.
+
+    :param ui.label: The label class from NiceGUI.
+    """
+
+    def _handle_text_change(self, text: str) -> None:
+        """
+        This function is used to change the text of the header label.
+
+        :param str text: The text to be displayed in the header label.
+        :return: None
+        """
+        super()._handle_text_change(text)
+
+
+class SliderValue(ui.slider):
+    """
+    This class is a subclass of NiceGUI's label class. It is used to display the header of the page.
+
+    :param ui.label: The label class from NiceGUI.
+    """
+
+    def _handle_value_change(self, value: int) -> None:
+        """
+        This function is used to change the text of the header label.
+
+        :param str text: The text to be displayed in the header label.
+        :return: None
+        """
+        super()._handle_value_change(value)
