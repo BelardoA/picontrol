90aa4b347fa00164c1bd4a14c61451a2d491b1b6
BelardoA <livintodie4e@gmail.com>
2024-01-15 20:46:45 -0500

    added method to create confirmation boxes

--- picontrol/picontrol_web_app/utils/__init__.py
+++ picontrol/picontrol_web_app/utils/__init__.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
 """Utilities for PiControl web app."""
 
+from nicegui import ui
 from .dynamic_objects import DynamicLabel, SliderValue
 from .button_tables import ButtonTable
+
+
+def create_confirmation(title: str, message: str, on_click: callable) -> ui.dialog:
+    """
+    Function to create a confirmation dialog box.
+
+    :param str title: The title of the dialog box.
+    :param str message: The message to be displayed in the dialog box.
+    :param callable on_click: The function to be called when the user clicks the yes button.
+    :return: Confirmation dialog box.
+    :rtype: ui.dialog
+    """
+    with ui.dialog() as dialog, ui.card():
+        ui.label(title).classes("text-xl")
+        ui.separator()
+        ui.label(message)
+        with ui.row().classes('ml-auto'):
+            ui.button("Yes", color="red", on_click=lambda: on_click(dialog))
+            ui.button("No", on_click=lambda: dialog.close())
+    return dialog
